ebc612753bd53aa93b86624c0ebe41a8f76ee00a
Kristen Prescott <prescottdarling@gmail.com>
2021-06-03 00:03:24 -0400

    styling changes

--- client/src/pages/PlaylistGenerator.js
+++ client/src/pages/PlaylistGenerator.js
@@ -2,5 +2,11 @@
 
 export default function PlaylistGenerator() {
   //
-  return <div className="PlaylistGenerator">You are logged in.</div>;
+  return (
+    <div className="PlaylistGenerator">
+      {/* <p className="title">MoodRing</p> */}
+
+      <p className="loggedin">You are logged in.</p>
+    </div>
+  );
 }
